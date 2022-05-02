cc892fa0f3f4c64790a3520a639c2523c329eb76
jellysoo97 <jellysoo97@gmail.com>
2022-05-03 00:55:26 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react"
+import { Routes, Route } from "react-router-dom"
+
+import LandingPage from "./components/LandingPage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense fallback={<div>Loading...</div>}>
+      <div style={{ minHeight: "calc(100vh - 80px)" }}>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Suspense>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/LandingPage.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "../index.css"
+
+function LandingPage() {
+  return <div>initial setting</div>
+}
+
+export default LandingPage
